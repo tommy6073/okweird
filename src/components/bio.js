fb91aef9b1d03fa647179f0ea593aebfa3a3ebe8
Takayuki NAGATOMI <tommyt6073@gmail.com>
2021-03-11 16:45:01 +0900

    Upgrade Gatsby to v3

--- src/components/bio.js
+++ src/components/bio.js
@@ -7,45 +7,41 @@
 
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import Image from "gatsby-image"
+import { GatsbyImage } from "gatsby-plugin-image";
 
 const Bio = () => {
-  const data = useStaticQuery(graphql`
-    query BioQuery {
-      avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
-        childImageSharp {
-          fixed(width: 50, height: 50, quality: 95) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-      site {
-        siteMetadata {
-          author {
-            name
-            summary
-          }
-        }
+  const data = useStaticQuery(graphql`query BioQuery {
+  avatar: file(absolutePath: {regex: "/profile-pic.jpg/"}) {
+    childImageSharp {
+      gatsbyImageData(width: 50, height: 50, quality: 95, layout: FIXED)
+    }
+  }
+  site {
+    siteMetadata {
+      author {
+        name
+        summary
       }
     }
-  `)
+  }
+}
+`)
 
   // Set these values by editing "siteMetadata" in gatsby-config.js
   const author = data.site.siteMetadata?.author
 
-  const avatar = data?.avatar?.childImageSharp?.fixed
+  const avatar = data?.avatar?.childImageSharp?.gatsbyImageData
 
   return (
     <div className="bio">
       {avatar && (
-        <Image
-          fixed={avatar}
+        <GatsbyImage
+          image={avatar}
           alt={author?.name || ``}
           className="bio-avatar"
           imgStyle={{
             borderRadius: `50%`
-          }}
-        />
+          }} />
       )}
       {author?.name && (
         <div>
@@ -97,7 +93,7 @@ const Bio = () => {
         </div>
       )}
     </div>
-  )
+  );
 }
 
 export default Bio
